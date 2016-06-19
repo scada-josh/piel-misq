5975061fa0c6aa499809d1f14ce23fe981d53651
scada-josh <scada.josh@gmail.com>
2016-06-19 22:47:30 +0700

    2016-06-19 iel-mis

---
+++ config.rb
@@ -0,0 +1,77 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :phase_environment, 'Development' # Development, Test, Production 
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+set :helper_dir, 'helper'
+
+set :partials_desktop_home, 'src/desktop/home'
+set :partials_desktop_home_kpi_operation_operationPerformance, 'src/desktop/home/menu-kpi/menu-operation/menu-operation-performance'
+
+
+
+# Reload the browser automatically whenever files change
+ configure :development do
+#   activate :livereload
+	activate :php
+    activate :bower
+ end
+
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+  activate :php
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Any files you want to ignore:
+  ignore '/helper/*'
+end
+
+
+
+# config.rb
+# Add bower's directory to sprockets asset path
+after_configuration do
+  #@bower_config = JSON.parse(IO.read("#{root}.bowerrc"))
+  #sprockets.append_path File.join "#{root}", @bower_config["directory"]
+  sprockets.append_path File.join "#{root}", "bower_components"
+  sprockets.append_path File.join "#{root}", "source"
+  #sprockets.import_asset 'jquery'
+end
